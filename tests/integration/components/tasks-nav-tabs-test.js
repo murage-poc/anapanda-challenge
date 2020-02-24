6ec7755cbcb2f9ed466a4850aa94c5f079278e57
mimidots <murage@mimidots.com>
2020-02-24 05:10:18 +0300

    Fix linter errors

--- tests/integration/components/tasks-nav-tabs-test.js
+++ tests/integration/components/tasks-nav-tabs-test.js
@@ -1,7 +1,5 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | tasks-nav-tabs', function(hooks) {
   setupRenderingTest(hooks);
