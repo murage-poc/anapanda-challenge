015bb53b79b469f1964b436b0de0b85b53739dda
mimidots <murage@mimidots.com>
2020-02-24 05:24:29 +0300

    .

--- tests/integration/components/toggle-done-test.js
+++ tests/integration/components/toggle-done-test.js
@@ -1,25 +1,8 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | toggle-done', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<ToggleDone />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <ToggleDone>
-      </ToggleDone>
-    `);
-
-    assert.equal(this.element.textContent.trim(), '');
-  });
 });
